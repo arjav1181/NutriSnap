176db21c8883ac858bfab75fcb3801f302796ea0
Arjav Jain <arjav.3003jain@gmail.com>
2025-11-20 11:05:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4575196262-59be4",
+  "appId": "1:417526887172:web:bc9e9b6c6e05cabcfbd651",
+  "apiKey": "AIzaSyArCxaqaDDweKNsMdBLlvHtgCxUNTNUhdE",
+  "authDomain": "studio-4575196262-59be4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "417526887172"
+};
